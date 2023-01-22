1877d7f7a07c156afa1f8996ff0b6234ded3c57d
anandsahujnu11 <anandsahujnu11@gmail.com>
2023-01-23 00:58:38 +0530

    added feature selection methods

--- modelling.py
+++ modelling.py
@@ -1,3 +1,7 @@
+import seaborn
+import smogn
+import pandas as pd
+
 def build_ml_model():
     """
     This module helps in fitting to all the ml and dl algorithms that are available in Scikit-learn
@@ -27,18 +31,101 @@ def build_ml_model():
 
 class feature_engineering:
     
-    def correlation_method():
-        pass
+    def feature_thru_wrapper(dataset:str,target_name:str,feat_selc_dirn:str,num_features:int,model:callable,score_param:str,cross_val:int):
+        """
+        Function to select features through feature selection method
+
+        Parameter
+        ---------
+        dataset : training dataset
+        target_name: name of the target variable
+        feat_selc_dirn: SFS, SFFS, SBS,SBFS
+        num_features: number_features_to_keep
+        model: model to fit to select features
+        score_param: neg_mean_squared_error, r2, accuracy
+        cross_val: KFold(n_splits=5,shuffle=True, random_state=False))
+
+        """
+        
+        from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+
+        dataset=dataset._get_numeric_data()
+        features=dataset.drop([target_name], axis = 1)
+        target = dataset.loc[:,target_name]
+
+        if feat_selc_dirn=='SFS':
+           forward_param=True
+           floating_param=False
+
+        elif feat_selc_dirn=='SBS':
+           forward_param=False
+           floating_param=False
+        
+        elif feat_selc_dirn=='SFFS':
+           forward_param=True
+           floating_param=True
+        
+        elif feat_selc_dirn=='SBFS':
+           forward_param=False
+           floating_param=True
+
+
+        sfs = SFS(model, 
+           k_features=num_features, 
+           forward=forward_param, 
+           floating=floating_param, 
+           scoring=score_param,
+           cv=cross_val)
+
+        sfs = sfs.fit(features, target)
+
+        print("\nfeature_pred_score :",sfs.k_score_)
+        print("\nfeatures_name :",sfs.k_feature_idx_)
+        print("\nfeatures_name :",sfs.k_feature_names_)
+
+        Relevant_Features =dataset.loc[:, sfs.k_feature_names_]
+
+
+        return Relevant_Features
+
+
+    def feature_thru_correlation(dataset, target, lower_threshold, corr_method):
+        """
+        corr_method = pearson, kendall, spearman
+
+        """
+        dataset=dataset.reset_index()
+        dataset = dataset[dataset.columns.drop((dataset.filter(regex='ndex')))]
+        dataset=dataset._get_numeric_data()
+        matrix = abs(dataset.corr(method=corr_method,numeric_only = True))[target].sort_values(kind="quicksort", ascending=False)
+        matrix = matrix[matrix > lower_threshold]
+        print("\nCorrelated Features :", matrix)
+
+        Relevant_Features =dataset.loc[:, abs(dataset.corr(method=corr_method,numeric_only = True)[target]) > lower_threshold]
+        Relevant_Features = Relevant_Features[Relevant_Features.columns.drop((Relevant_Features.filter(regex='ndex')))]
+        Relevant_Features = Relevant_Features[Relevant_Features.columns.drop((Relevant_Features.filter(regex='unnamed')))]
+        return Relevant_Features
+        
+    def generate_synthetic_data(dataset, target, k_value, samp, thres, rel, rel_type,coef):
     
-    def correlation_method():
-        pass
+        df = smogn.smoter(
 
-    def generate_synthetic_data():
-        pass
-    
+            ## main arguments
+            data = dataset,             ## pandas dataframe
+            y = target,                 ## string ('header name')
+            k = k_value,                ## positive integer (k < n)
+            samp_method = samp,         ## string ('balance' or 'extreme')
 
-    pass
+            ## phi relevance arguments
+            rel_thres = thres,          ## positive real number (0 < R < 1)
+            rel_method = rel,           ## string ('auto' or 'manual')
+            rel_xtrm_type = rel_type,   ## string ('low' or 'both' or 'high')
+            rel_coef = coef             ## positive real number (0 < R)
 
+        )
+        seaborn.kdeplot(dataset[target], label = "Original")
+        seaborn.kdeplot(df[target], label = "SMOGN")        
+        return df
 
 
 class modelling:
