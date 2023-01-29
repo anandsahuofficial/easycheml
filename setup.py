277de79cbb072e1c49cc179da29b9a69147066de
anandsahujnu11 <anandsahujnu11@gmail.com>
2023-01-29 17:41:10 +0530

    transferred script files to easycheml  folder

--- setup.py
+++ setup.py
@@ -5,19 +5,25 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-     name='easychemai',  
+     name='easycheml',  
      version='0.1',
-     scripts=['easychemai'] ,
      author="Anand Sahu",
      author_email="anandsahuofficial@gmail.com",
      description="A simple tool for using artificial intelligence in chemistry",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="",
-     packages=setuptools.find_packages(),
+     #packages=setuptools.find_packages(),
+     packages=['easycheml'],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
  )
+
+
+
+
+
+
